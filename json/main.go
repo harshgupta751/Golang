81f4dc0b087de8e4359b8cd951aa0dec06087300
harshgupta751 <harshachieve300@gmail.com>
2026-03-28 00:35:55 +0530

    Decoded Json

--- json/main.go
+++ json/main.go
@@ -14,12 +14,20 @@ type course struct {
 }
 
 func main() {
+// EncodeJson()
+decodeJson()
 
-	content := []course{
+
+
+}
+
+
+func EncodeJson(){
+		content := []course{
 		{"Harsh Gupta", 1200, "https://harsh.dev.com", "anyPassword", []string{"binary-search", "arrays", "NumberPattern"}},
 		{"Mohan Singh", 2000, "https://temp.dev.com", "tempPassword", []string{"linear-search", "arrays", "NumberPattern"}},
-		{"Raman", 1200, "https://harsh.dev.com", "anyPassword", []string{"binary-search", "arrays", "NumberPattern"}},
-		{"Raman", 1200, "https://harsh.dev.com", "anyPassword", nil},
+		{"Raman", 1200, "https://raman.dev.com", "dummyPassword", []string{"binary-search", "arrays", "NumberPattern"}},
+		{"Shyam", 1800, "https://shyam.dev.com", "any", nil},
 	}
 
 	jsonData, err := json.MarshalIndent(content, "", "\t")
@@ -29,5 +37,78 @@ func main() {
 	}
 
 	fmt.Printf("%s\n", jsonData)
+}
+
+
+func decodeJson(){
+
+jsonData := []byte(`
+[
+        {
+                "coursename": "Harsh Gupta",
+                "Price": 1200,
+                "platform": "https://harsh.dev.com",
+                "tags": [
+                        "binary-search",
+                        "arrays",
+                        "NumberPattern"
+                ]
+        },
+        {
+                "coursename": "Mohan Singh",
+                "Price": 2000,
+                "platform": "https://temp.dev.com",
+                "tags": [
+                        "linear-search",
+                        "arrays",
+                        "NumberPattern"
+                ]
+        },
+        {
+                "coursename": "Raman",
+                "Price": 1200,
+                "platform": "https://raman.dev.com",
+                "tags": [
+                        "binary-search",
+                        "arrays",
+                        "NumberPattern"
+                ]
+        },
+        {
+                "coursename": "Shyam",
+                "Price": 1800,
+                "platform": "https://shyam.dev.com"
+        }
+]
+`)
+
+check := json.Valid(jsonData)
+
+if check {
+	fmt.Println("Json is valid")
+} else {
+	fmt.Println("Json is Invalid")
+}
+
+var data []course
+
+ json.Unmarshal(jsonData, &data)
+
+	 fmt.Printf("%#v", data)
+	fmt.Println()
+
+	var MapData []map[string]interface{}
+
+	json.Unmarshal(jsonData, &MapData)
+
+fmt.Println(MapData)
+
+	fmt.Println()
+
+	for k, v := range MapData[0] {
+		fmt.Printf("Key is: %v, Value is: %v, Type is: %T\n", k, v, v)
+	}
 
 }
+
+
